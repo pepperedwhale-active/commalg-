281a13009dfd7dafc631527262b146b68705a04d
aniruda suswaram <100919445+pepperedwhale-active@users.noreply.github.com>
2025-03-18 01:59:34 +0530

    Update gui.py
    cleaning up the botched code...

--- gui.py
+++ gui.py
@@ -1,6 +1,6 @@
 import tkinter as tk
 import math
-import patideal 
+import pathideal as patideal
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
